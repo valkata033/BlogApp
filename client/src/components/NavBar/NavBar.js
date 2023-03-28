192bf01d4673d93c1d32a6da1c16581e15d2f881
Valentin <valkata033@abv.bg>
2023-03-28 12:40:53 +0300

    Fixed navbar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -5,50 +5,39 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 
+import styles from './NavBar.module.css';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
 import { useContext } from 'react';
 
 export const NavBar = () => {
-    const {isAuthenticated, userEmail} = useContext(AuthContext);
+    const { isAuthenticated, userEmail } = useContext(AuthContext);
 
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
                 <Navbar.Brand href="/"><FontAwesomeIcon icon={faBlog} />  Blog App</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    {/*<Nav className="me-auto">
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                            Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                            Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>*/}
-                    <Nav>
-                        <Nav.Link href="/">Home</Nav.Link>
-                        {isAuthenticated && (
-                            <>
-                                <Nav.Link>{userEmail}</Nav.Link>
-                                <Nav.Link href="/catalog">Catalog</Nav.Link>
-                                <Nav.Link href="/logout">Logout</Nav.Link>
-                            </>
-                        )}
+               
+                <nav id='navbar'>
+                    {isAuthenticated && (
+                        <div id="user">
+                            <Link className={styles.btnNav} to="/">Home</Link>
+                            <Link className={styles.btnNav} to="/create-post">Create Post</Link>
+                            <Link className={styles.btnNav} to="/userInfo">{userEmail}</Link>
+                            <Link className={styles.btnNav} to="/logout">Logout</Link>
+                        </div>
+                    )}
 
-                        {!isAuthenticated && (
-                            <>
-                                <Nav.Link href="/login">Login</Nav.Link>
-                                <Nav.Link href="/register">Register</Nav.Link>
-                            </>
-                        )}
-                        
-                    </Nav>
-                </Navbar.Collapse>
+                    {!isAuthenticated && (
+                        <div id="guest">
+                            <Link className={styles.btnNav} to="/">Home</Link>
+                            <Link className={styles.btnNav} to="/login">Login</Link>
+                            <Link className={styles.btnNav} to="/register">Register</Link>
+                        </div>
+                    )}
+                </nav>
+
             </Container>
         </Navbar>
     );
